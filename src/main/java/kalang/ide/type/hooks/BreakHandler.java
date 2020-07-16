f2ab57bea7a043dba5dbeaeccc64caa62d571e03
kasonyang <me@kason.site>
2020-07-16 23:25:58 +0800

    fix: break handler

--- src/main/java/kalang/ide/type/hooks/BreakHandler.java
+++ src/main/java/kalang/ide/type/hooks/BreakHandler.java
@@ -4,9 +4,13 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import kalang.ide.KaLanguage;
 import kalang.ide.Logger;
+import kalang.ide.lexer.KaTokenId;
 import kalang.ide.utils.DocumentUtil;
 import org.netbeans.api.editor.mimelookup.MimePath;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenHierarchy;
+import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.spi.editor.typinghooks.TypedBreakInterceptor;
 
 /**
@@ -30,11 +34,11 @@ public class BreakHandler implements TypedBreakInterceptor {
         }
         Document doc = context.getDocument();
         String preChar = doc.getText(preCaretOffset, 1);
-        Logger.log("prechar=" + preChar);
-        if ("{".equals(preChar)){
-            String ident = DocumentUtil.getIndent(doc, preCaretOffset);
-            String text = "\n" + ident + "\n" + ident + "}";
-            context.setText(text, 0, 1+ident.length());
+        Logger.log("pre char=" + preChar);
+        if ("{".equals(preChar) && isMissingRightBracket(doc)){
+            String indent = DocumentUtil.getIndent(doc, preCaretOffset);
+            String text = "\n" + indent + "\n" + indent + "}";
+            context.setText(text, 0, 1+indent.length());
         }
     }
 
@@ -44,6 +48,27 @@ public class BreakHandler implements TypedBreakInterceptor {
 
     @Override
     public void cancelled(Context context) {
+    }
+
+    private boolean isMissingRightBracket(Document doc) {
+        TokenHierarchy<Document> th = TokenHierarchy.get(doc);
+        TokenSequence<KaTokenId> ts = (TokenSequence<KaTokenId>) th.tokenSequence();
+        int leftCount = 0;
+        int rightCount = 0;
+        ts.moveStart();
+        while (ts.moveNext()) {
+            Token<KaTokenId> token = ts.token();
+            if (token.text().length() != 1) {
+                continue;
+            }
+            String tokenText = token.text().toString();
+            if ("{".equals(tokenText)) {
+                leftCount ++;
+            } else if ("}".equals(tokenText)) {
+                rightCount ++;
+            }
+        }
+        return leftCount > rightCount;
     }
     
     @MimeRegistration(mimeType = KaLanguage.MIME_TYPE, service = TypedBreakInterceptor.Factory.class)
