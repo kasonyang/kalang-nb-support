cd0a5214a1500e5ab2d72f7e72c2ac610b6095d5
kasonyang <me@kason.site>
2020-07-16 23:27:34 +0800

    feat: support bracket skip

--- src/main/java/kalang/ide/type/hooks/BracketCompleter.java
+++ src/main/java/kalang/ide/type/hooks/BracketCompleter.java
@@ -1,6 +1,9 @@
 package kalang.ide.type.hooks;
 
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Caret;
+import javax.swing.text.Document;
+
 import kalang.ide.KaLanguage;
 import kalang.ide.Logger;
 import org.netbeans.api.editor.mimelookup.MimePath;
@@ -13,6 +16,8 @@ import org.netbeans.spi.editor.typinghooks.TypedTextInterceptor;
  */
 public class BracketCompleter implements TypedTextInterceptor {
 
+    private final static char[] COMPLETE_KEYS = "()[]".toCharArray();
+
     @Override
     public boolean beforeInsert(Context context) throws BadLocationException {
         return false;
@@ -20,48 +25,62 @@ public class BracketCompleter implements TypedTextInterceptor {
 
     @Override
     public void insert(MutableContext context) throws BadLocationException {
-        int offset = context.getOffset();
         String text = context.getText();
-        Logger.log("insert:typed=" + text);
-        Logger.log("insert:offset=" + offset);
-        if (this.isCompleteKeyword(text)) {
-            String completeText = this.getCompleteText(text);
-            Logger.log("insert:completeText=" + completeText);
-            if (!completeText.isEmpty()) {
-                String newText = text + completeText;
-                context.setText(newText, text.length());
-            }
+        char[] chars = text.toCharArray();
+        if (chars.length != 1) {
+            return;
+        }
+        char ch = chars[0];
+        String completeText = getCompleteText(ch);
+        if (!completeText.isEmpty()) {
+            String newText = text + completeText;
+            context.setText(newText, text.length());
         }
     }
 
     @Override
     public void afterInsert(Context context) throws BadLocationException {
-
+        Caret caret = context.getComponent().getCaret();
+        Document doc = context.getDocument();
+        int offset = context.getOffset();
+        char[] chars = context.getText().toCharArray();
+        if (chars.length != 1) {
+            return;
+        }
+        char ch = chars[0];
+        if (!isCompletedChar(ch)) {
+            return;
+        }
+        char currentChar = context.getDocument().getText(offset, 1).charAt(0);
+        if (currentChar != ch) {
+            return;
+        }
+        doc.remove(offset, 1);
+        caret.setDot(offset + 1);
     }
 
     @Override
     public void cancelled(Context context) {
     }
 
-    private boolean isCompleteKeyword(String input) {
-        String[] keywords = new String[]{"[", "("};
-        for (String k : keywords) {
-            if (k.equals(input)) {
+    private boolean isCompletedChar(char ch) {
+        for (int i = 1; i < COMPLETE_KEYS.length; i += 2) {
+            if (ch == COMPLETE_KEYS[i]) {
                 return true;
             }
         }
         return false;
     }
 
-    private String getCompleteText(String input) {
-        if ("[".equals(input)) {
-            return "]";
-        } else if ("(".equals(input)) {
-            return ")";
-        } else {
-            return "";
+    private String getCompleteText(char ch) {
+        for (int i = 0; i < COMPLETE_KEYS.length; i += 2) {
+            if (COMPLETE_KEYS[i] == ch) {
+                return new String(COMPLETE_KEYS, i+1, 1);
+            }
         }
+        return "";
     }
+
     
     @MimeRegistration(mimeType = KaLanguage.MIME_TYPE, service = TypedTextInterceptor.Factory.class)
     public static class Factory implements TypedTextInterceptor.Factory {
